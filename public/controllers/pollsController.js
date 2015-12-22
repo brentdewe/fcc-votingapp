2271bcb96d1f6a9f61d8fcdbdc07495948de0b32
Brent De Weerdt <deweerdt.brent@gmail.com>
2015-12-22 23:01:08 +0100

    Fetch polls from the api

--- public/controllers/pollsController.js
+++ public/controllers/pollsController.js
@@ -1,14 +1,7 @@
 var pollsApp = angular.module('pollsApp', []);
 
-pollsApp.controller('PollsCtrl', ['$scope', function($scope) {
-	$scope.polls = [{
-		"_id":"56785579c16682f00b3661aa",
-		"title":"Test poll",
-		"description":"poll to test",
-		"owner":"567845e11689918b0316043f",
-		"items":[
-			{"text":"option one","votes":0},
-			{"text":"option two","votes":1}
-		]
-	}];
+pollsApp.controller('PollsCtrl', ['$scope', '$http', function($scope, $http) {
+	$http.get('/api/polls/search').then(function(res) {
+		$scope.polls = res.data;
+	});
 }]);
