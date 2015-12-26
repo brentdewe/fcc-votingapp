cd4df727401be728c7ab0e75f6adb55850df12d4
Brent De Weerdt <deweerdt.brent@gmail.com>
2015-12-26 18:09:01 +0100

    Do not use _id for poll items

--- app/models/poll.js
+++ app/models/poll.js
@@ -2,14 +2,16 @@
 
 var mongoose = require('mongoose');
 
+var item = mongoose.Schema({
+	text: String,
+	votes: { type: Number, default: 0 }
+}, { _id: false });
+
 var poll = mongoose.Schema({
 	owner: mongoose.Schema.ObjectId,
 	title: String,
 	description: String,
-	items: [{
-		text: String,
-		votes: { type: Number, default: 0 }
-	}]
+	items: [item]
 });
 
 module.exports = mongoose.model('Poll', poll);
