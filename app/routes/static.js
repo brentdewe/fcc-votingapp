9e39a462cc4e8a3d1e7642aec769cbf1dd807d20
Brent De Weerdt <deweerdt.brent@gmail.com>
2015-12-30 23:59:48 +0100

    Add build scripts

--- app/routes/static.js
+++ app/routes/static.js
@@ -7,9 +7,9 @@ module.exports = function(app) {
 			'node_modules/angular/angular.min.js'));
 	app.use('/angular-route.js', express.static(
 			'node_modules/angular-route/angular-route.min.js'));
-	app.use('/modules', express.static('client/modules'));
+	app.use('/modules', express.static('client/dist/modules'));
 
 	app.get('/', function(req, res) {
-		res.sendFile(process.cwd() + '/client/index.html');
+		res.sendFile(process.cwd() + '/client/dist/modules/index.html');
 	});
 }
--- client/modules/app.js
+++ client/modules/app.js
@@ -5,7 +5,7 @@ var app = angular.module('app', ['login', 'polls', 'ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl: '/modules/index.html'
+		templateUrl: '/modules/start.html'
 	});
 }]);
 
