5e477a022b9bde2a4ae375483a7a75758e06055c
Brent De Weerdt <deweerdt.brent@gmail.com>
2016-01-16 16:07:11 +0100

    Remove old login url

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,17 +4,15 @@ module.exports = function(app, passport) {
 
 	app.use('/api', require('./api'));
 
-	app.get('/login', function(req, res) { res.redirect('/login.html') });
-
 	app.get('/auth/github', passport.authenticate('github'));
 
 	app.get('/auth/github/callback', passport.authenticate('github', {
 		successRedirect: '/',
-		failureRedirect: '/login'
+		failureRedirect: '/#/login'
 	}));
 
 	app.post('/auth/local', passport.authenticate('local', {
 		successRedirect: '/',
-		failureRedirect: '/login'
+		failureRedirect: '/#/login'
 	}));
 }
